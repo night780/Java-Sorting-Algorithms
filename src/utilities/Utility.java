8a9f01dc076eb7af26a943c4989868b7ab17bb70
night780 <96319709+night780@users.noreply.github.com>
2022-10-09 16:31:12 -0700

    JavaDoc

--- src/utilities/Utility.java
+++ src/utilities/Utility.java
@@ -2,7 +2,18 @@ package utilities;
 
 import java.util.Random;
 
+/**
+ *
+ * @author Jacob J
+ * @version 1.0
+ */
 public class Utility {
+    /**
+     * If the array is sorted, then there are no inversions
+     *
+     * @param array the array to check for inversions
+     * @return A boolean value.
+     */
     public static boolean containsInversions(int[] array) {
         for (int i = 0; i < array.length-1; i++) {
             if (array[i]>array[i+1]){
@@ -13,7 +24,14 @@ public class Utility {
         return false;
     }
 
-    //generate an array of arrraySize elements in the reange low and high bounds
+    /**
+     * It generates an array of random integers between lowBound and highBound
+     *
+     * @param arraySize the size of the array to be generated
+     * @param lowBound the lowest number that can be generated
+     * @param highbound the highest number that can be generated
+     * @return An array of random numbers
+     */
     public static int[] generateArray(int arraySize, int lowBound, int highbound) {
         int[] result = new int[arraySize];
 
@@ -27,6 +45,13 @@ public class Utility {
         return result;
     }
 
+    /**
+     * Swap the values at the first and second indices of the given array.
+     *
+     * @param array the array that you want to sort
+     * @param firstIndex the index of the first element in the array
+     * @param secondIndex the index of the second element to swap
+     */
     public static void swap(int[] array, int firstIndex, int secondIndex) {
         int temp = array[firstIndex];
         array[firstIndex]=array[secondIndex];
